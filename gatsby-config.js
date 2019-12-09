5f5eff1a7e11a685fdc39e4c048e3432893f8b0c
Thom Crowe <thom@thomcrowe.com>
2019-12-09 16:39:56 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Orthodox Youth and Technology',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Looking at Techology and What It Means in Our Christian Lives',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
